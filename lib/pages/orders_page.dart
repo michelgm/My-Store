d0dcb689c784f88d34e0e65f50ab0979c89a420c
Michel Mendonca <michelgm96@outlook.com>
2022-08-04 13:38:06 -0300

    primeiro commit

---
+++ lib/pages/orders_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/models/order_list.dart';
+
+import '../components/order.dart';
+
+class OrdersPage extends StatelessWidget {
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus pedidos'),
+      ),
+      drawer: AppDrawer(),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshOrders(context),
+        child: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return Center(
+                child: Text('Ocorreu um erro!'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+                ),
+              );
+            }
+          },
+        ),
+      ),
+
+      // body: RefreshIndicator(
+      //   onRefresh: () => _refreshOrders(context),
+      //   child: _isLoading
+      //       ? Center(child: CircularProgressIndicator())
+      //       : ListView.builder(
+      //           itemCount: orders.itemsCount,
+      //           itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+      //         ),
+      // ),
+    );
+  }
+}
